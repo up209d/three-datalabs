947a04a346b22941c749c61d01899dfa9a04d14b
up209d <up209d@gmail.com>
2019-03-07 13:32:31 +1100

    Clone from my BitBucket

--- base.config.js
+++ base.config.js
@@ -3,4 +3,4 @@
 // So the basename config to createBrowserHistory and all Router with basename will solve that problem
 
 // basename is for AJAX uses also
-module.exports = ''; // Should leave it blank if no using, otherwise it has to start with a slash
+module.exports = '/three-datalabs'; // Should leave it blank if no using, otherwise it has to start with a slash
